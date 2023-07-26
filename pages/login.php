8916b202cdefcd107d9333db9472520d792b7f2f
Moonclood <makaroninproduction@gmail.com>
2023-07-26 19:17:29 +0300

    Авторизация

--- pages/login.php
+++ pages/login.php
@@ -16,11 +16,14 @@ function login($method)
         }
         include "db.php";
         $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-        if ($user = $mysql->query("SELECT id FROM users WHERE email='$email'")) {
+        if ($user = $mysql->query("SELECT id, password FROM users WHERE email='$email'")) {
             $user = $user->fetch_assoc();
-            $_SESSION['id'] = $user['id'];
-            echo "Успешная авторизация!";
+            if (password_verify($_POST['password'], $user['password'])) {
+                $_SESSION['id'] = $user['id'];
+                echo "Успешная авторизация!";
+            } else {
+                echo "Неправильный пароль!";
+            }
         }
         $mysql->close();
     }
